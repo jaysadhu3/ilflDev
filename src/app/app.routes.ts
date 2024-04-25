c568b71e05c2c76dc31f1af46ea24f9b7b04a9d6
Jay Sadhu <68586992+JaySadhu99@users.noreply.github.com>
2024-04-26 00:17:18 +0530

    new about , contact , home page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { ContactUsComponent } from './public/contact-us/contact-us.component';
+import { AboutComponent } from './public/about/about.component';
 
 export const routes: Routes = [
     { path : '' , component : HomeComponent},
     { path : 'Home' , component : HomeComponent},
-    { path : 'Contact-Us' , component : ContactUsComponent}
+    { path : 'Contact-Us' , component : ContactUsComponent},
+    { path : 'About' , component : AboutComponent}
 ];
--- src/app/public/contact-us/contact-us.component.ts
+++ src/app/public/contact-us/contact-us.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from "../about/about.component";
+import { HeaderComponent } from "../../common/header/header.component";
 
 @Component({
-  selector: 'app-contact-us',
-  standalone: true,
-  imports: [],
-  templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+    selector: 'app-contact-us',
+    standalone: true,
+    templateUrl: './contact-us.component.html',
+    styleUrl: './contact-us.component.css',
+    imports: [HeaderComponent]
 })
 export class ContactUsComponent {
 
