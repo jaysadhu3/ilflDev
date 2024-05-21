44257633c73b4f89ee3438b8dc684ee378a7ff19
Jay Sadhu <68586992+JaySadhu99@users.noreply.github.com>
2024-05-21 21:27:40 +0530

    deployable changes added

--- ilfl.Web/src/app/app.module.ts
+++ ilfl.Web/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { RouterOutlet } from "@angular/router";
+import { RouterModule, RouterOutlet } from "@angular/router";
 import { HttpRequestInterceptor } from "./interceptor/interceptor";
+import { LocationStrategy, HashLocationStrategy } from "@angular/common";
+import { routes } from "./app.routes";
 
 @NgModule({
     declarations: [
     ],
     imports: [
+        RouterModule.forRoot(routes),
         RouterOutlet,
         ReactiveFormsModule,
         FormsModule,
@@ -18,8 +21,12 @@ import { HttpRequestInterceptor } from "./interceptor/interceptor";
             provide: HTTP_INTERCEPTORS,
             useClass: HttpRequestInterceptor,
             multi: true
+        }, {
+            provide: LocationStrategy,
+            useClass: HashLocationStrategy
         }
     ],
+    exports: [RouterModule],
     bootstrap: [],
     schemas: [
         CUSTOM_ELEMENTS_SCHEMA
--- ilfl.Web/src/app/app.routes.ts
+++ ilfl.Web/src/app/app.routes.ts
@@ -1,19 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './public/home/home.component';
-import { ContactUsComponent } from './public/contact-us/contact-us.component';
-import { AboutComponent } from './public/about/about.component';
-import { InvestorsRelationsComponent } from './public/investors-relations/investors-relations.component';
-import { AnnualReportComponent } from './public/annual-report/annual-report.component';
-import { AuditedAccountsOfSubsidiariesComponent } from './public/audited-accounts-of-subsidiaries/audited-accounts-of-subsidiaries.component';
-import { EVotingResultsComponent } from './public/e-voting-results/e-voting-results.component';
-import { NoticeToShareholdersComponent } from './public/notice-to-shareholders/notice-to-shareholders.component';
-import { TransferOfSharesToIEPFComponent } from './public/transfer-of-shares-to-iepf/transfer-of-shares-to-iepf.component';
-import { UnclaimedDividendComponent } from './public/unclaimed-dividend/unclaimed-dividend.component';
-import { CorporateOverviewComponent } from './public/corporate-overview/corporate-overview.component';
-import { FormMgt7Component } from './public/form-mgt-7/form-mgt-7.component';
-import { BoardOfDirectorsComponent } from './public/board-of-directors/board-of-directors.component';
-import { ResignationOfDirectorComponent } from './public/resignation-of-director/resignation-of-director.component';
-import { CorporateSocialResponsibilityComponent } from './public/corporate-social-responsibility/corporate-social-responsibility.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { LoginComponent } from './admin/login/login.component';
 import { SaveContentComponent } from './admin/save-content/save-content.component';
@@ -24,13 +9,17 @@ import { ViewMenuComponent } from './admin/view-menu/view-menu.component';
 import { SavePageContentComponent } from './admin/save-page-content/save-page-content.component';
 
 export const routes: Routes = [
-    { path : '' , component : MainComponent},
-    { path : 'Admin/Dashboard' , component : DashboardComponent},
-    { path : 'Admin/AddContent' , component : SaveContentComponent},
-    { path : 'Admin' , component : LoginComponent},
-    { path : 'Admin/AddMenu' , component : SaveSectionComponent},
-    { path : 'Admin/ViewContent' , component : ViewContentComponent},
-    { path : 'Admin/ViewMenu' , component : ViewMenuComponent},
-    { path : 'Admin/savePageContent' , component : SavePageContentComponent},
-    { path : '**' , redirectTo: '', pathMatch: 'full'},
+    { path: '', component: MainComponent },
+    { path: 'Admin', component: LoginComponent },
+    {
+        path: 'Admin', children: [
+            { path: 'Dashboard', component: DashboardComponent },
+            { path: 'AddContent', component: SaveContentComponent },
+            { path: 'AddMenu', component: SaveSectionComponent },
+            { path: 'ViewContent', component: ViewContentComponent },
+            { path: 'ViewMenu', component: ViewMenuComponent },
+            { path: 'SavePageContent', component: SavePageContentComponent },
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
